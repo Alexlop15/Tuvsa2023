2fcae6d6f7c309b778b2ad087c686edeeb8c9b31
pc utvf <21307019@utfv.edu.mx>
2023-07-17 19:06:54 -0500

    17072023

---
+++ res.php
@@ -0,0 +1,17 @@
+<?php  
+error_reporting(E_ALL ^ E_DEPRECATED )
+header(`content-Type: text/html; Charset=UTF-8`);
+$email = isset($_POST[`email`]) ? $_POST[`email`] : ``;
+$pass = ($_POST[`pass`]) ? $_POST[`pass`] : ``;
+
+use PHPMailer\
+
+echo $correo;
+echo `<br>`;
+echo $correo;
+
+
+
+
+
+?>
